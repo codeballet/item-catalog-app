8abfff534eea40f332b5a27a7f118c1ec4a6a06f
johan <stjernholm@msn.com>
2019-06-23 00:28:39 +0800

    Chore: flask environment set to production

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    FLASK_ENV = 'development'
+    # FLASK_ENV = 'development'
     DATABASE_URL = os.environ.get('DATABASE_URL')
